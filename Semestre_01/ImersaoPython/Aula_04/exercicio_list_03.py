429c3db6e500cc728995af4c8b040a73a8b68825
RogerioTiuma <rogeriotiuma@hotmail.com>
2023-10-04 20:27:48 -0300

    Atualizacão_List_03

---
+++ Semestre_01/ImersaoPython/Aula_04/exercicio_list_03.py
@@ -0,0 +1,7 @@
+"""Faça um programa em duas partes:
+○ Parte 01: Ler 10 números inteiros e armazená-los em uma lista única.
+○ Parte 02: Em uma nova estrutura de repetição, armazene os números pares na
+lista PARES e os números ímpares na lista ÍMPARES.
+○ Imprima as três listas."""
+
+
