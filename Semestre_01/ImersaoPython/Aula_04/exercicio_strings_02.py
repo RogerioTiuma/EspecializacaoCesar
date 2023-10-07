29cddd0089e5099a51bcd84c6808e42419947b34
RogerioTiuma <rogeriotiuma@hotmail.com>
2023-10-07 07:51:04 -0300

    Tuplas_&_Dicionários

--- Semestre_01/ImersaoPython/Aula_04/exercicio_strings_02.py
+++ Semestre_01/ImersaoPython/Aula_04/exercicio_strings_02.py
@@ -1,8 +1,7 @@
 """2. Faça um programa que peça ao usuário para inserir:
 ○ uma frase
 ○ uma palavra que está contida na frase
-○ outra palavra que ele deseja substituir no lugar da primeira palavra inserida.
-Por fim, o programa exibe a frase modificada, toda em letra maiúscula."""
+○ outra palavra que ele deseja substituir no lugar da primeira palavra inserida. Por fim, o programa exibe a frase modificada, toda em letra maiúscula."""
 
 frase = input("Digite uma frase: ")
 busca = input("Digite uma palavra para buscar na frase: ")
